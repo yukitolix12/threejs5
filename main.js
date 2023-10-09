5d1ed0cb0ccd4f8de27af32dc00e7be96f8bf659
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-09 10:50:13 +0900

    sizeを指定

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 import * as THREE from "./build/three.module.js";
 import { OrbitControls } from "./controls/OrbitControls.js";
+
+//size
+const size = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
